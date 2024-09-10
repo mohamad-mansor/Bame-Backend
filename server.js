ed2abe3536cb377f561fe3592ca7e554e596d0f8
Devcodino <devcodino@proton.me>
2024-09-10 16:58:37 +0200

    Update

--- server.js
+++ server.js
@@ -25,32 +25,3 @@ app.use("/user", UserRouter);
 app.listen(3000, () => {
   console.log("Server is started on Port 3000");
 });
-
-// Endpoints
-// user (get, post, update, delete)
-// game (get, post, update, delete)
-
-//MySQL Datenbank
-/*  Spalte User 
-      - username
-      - email
-      - id
-      - password
-      - csrf
-      - role
-
-    Spalte Games
-      - title
-      - release
-      - Genre
-      - price
-      - publisher
-      - developer
-
-*/
-
-//Datenbank Verbindung
-// Herstellen der Verbindung
-
-// Note
-//mysql2 installieren
