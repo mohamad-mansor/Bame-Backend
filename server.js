d7be354466375a89545fb0ca1150eb096218043c
Devcodino <devcodino@proton.me>
2024-09-10 14:35:40 +0200

    Update

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 import express from "express";
-
 const app = express();
+
+// Endpoints
+// user (get, post, update, delete)
+// game (get, post, update, delete)
+
+//MySQL Datenbank
+/*  Spalte User 
+      - username
+      - email
+      - id
+      - password
+      - csrf
+      - role
+
+    Spalte Games
+      - title
+      - release
+      - Genre
+      - price
+      - publisher
+      - developer
+
+*/
+
+//Datenbank Verbindung
+// Herstellen der Verbindung
+
+// Note
+//mysql2 installieren
 
 app.listen(3000, () => {
   console.log("Server is started on Port 3000");
