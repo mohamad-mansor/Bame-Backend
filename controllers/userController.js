5df32150b89d4ae17244fe5310a2b4088ee9a84d
Devcodino <devcodino@proton.me>
2024-09-10 15:43:21 +0200

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,46 +1,27 @@
-const users = [];
-
-exports.getUser = (req, res) => {
-  const userId = req.params.id;
-  if (userId) {
-    const user = users.find((u) => u.id === parseInt(userId));
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).json({ message: "User not found" });
-    }
-  } else {
-    res.status(200).json(users);
-  }
+export const GetUserController = (req, res) => {
+  res.status(200).json({
+    code: 200,
+    answer: "Insert Logic",
+  });
 };
 
-exports.createUser = (req, res) => {
-  const newUser = {
-    id: users.length + 1,
-    ...req.body,
-  };
-  users.push(newUser);
-  res.status(201).json(newUser);
+export const PostUserController = (req, res) => {
+  res.status(200).json({
+    code: 200,
+    answer: "Insert Logic",
+  });
 };
 
-exports.updateUser = (req, res) => {
-  const userId = parseInt(req.params.id);
-  const userIndex = users.findIndex((u) => u.id === userId);
-  if (userIndex !== -1) {
-    users[userIndex] = { id: userId, ...req.body };
-    res.status(200).json(users[userIndex]);
-  } else {
-    res.status(404).json({ message: "User not found" });
-  }
+export const PutUserController = (req, res) => {
+  res.status(200).json({
+    code: 200,
+    answer: "Insert Logic",
+  });
 };
 
-exports.deleteUser = (req, res) => {
-  const userId = parseInt(req.params.id);
-  const userIndex = users.findIndex((u) => u.id === userId);
-  if (userIndex !== -1) {
-    const deletedUser = users.splice(userIndex, 1);
-    res.status(200).json(deletedUser);
-  } else {
-    res.status(404).json({ message: "User not found" });
-  }
+export const DeleteUserController = (req, res) => {
+  res.status(200).json({
+    code: 200,
+    answer: "Insert Logic",
+  });
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
+import { Router } from "express";
+import {
+  DeleteUserController,
+  GetUserController,
+  PostUserController,
+  PutUserController,
+} from "../controllers/userController.js";
 
-router.get("/:id?", userController.getUser);
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+export const UserRouter = Router();
 
-module.exports = router;
+UserRouter.get("/", GetUserController);
+UserRouter.post("/", PostUserController);
+UserRouter.put("/", PutUserController);
+UserRouter.delete("/", DeleteUserController);
