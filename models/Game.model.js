c461a773f2b165f187fdc3b87e0a7a4b57810b61
Devcodino <devcodino@proton.me>
2024-09-11 10:11:12 +0200

    See Commands to fix)

--- models/Game.model.js
+++ models/Game.model.js
@@ -1,12 +1,35 @@
 import { Schema, model } from "mongoose";
 
 const GameSchema = new Schema({
-  name: { type: String, unique: true, required: true }, // Game name
-  price: { type: Number, required: true }, // Price of the game
-  author: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Developer (Author)
-  publisher: { type: String, required: true }, // Publisher of the game
-  releaseDate: { type: Date, required: true }, // Release date of the game
-  age: { type: Number, required: true }, // Age restriction
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  }, // Game name
+  price: {
+    type: Number,
+    required: true,
+    // Add mindestprice (0)
+  }, // Price of the game
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  }, // Developer (Author)
+  publisher: {
+    type: String,
+    required: true,
+  }, // Publisher of the game
+  releaseDate: {
+    type: Date,
+    required: true,
+  }, // Release date of the game
+  age: {
+    type: Number,
+    required: true,
+    // Add mindestage (0)
+    // Add maxage (21)
+  }, // Age restriction
 });
 
 export const GameModel = model("Game", GameSchema);
