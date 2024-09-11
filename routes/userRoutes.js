44961e5709d9e5056e188ce5dd64f9f5d91e5a86
Ayhem <ayhem.alras@dci-student.org>
2024-09-11 10:35:15 +0200

    upd

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body, param } from 'express-validator';
 import {
   DeleteUserController,
   GetUserController,
@@ -12,3 +13,23 @@ UserRouter.get("/", GetUserController);
 UserRouter.post("/", PostUserController);
 UserRouter.put("/", PutUserController);
 UserRouter.delete("/", DeleteUserController);
+
+UserRouter.put('/:userid', [
+  param('userid').isInt().withMessage('User ID'),
+  body('email').isEmail().withMessage('Email'),
+  body('password').isLength({ min: 8 }).withMessage('Password'),
+  body('username').isLength({ min: 3 }).withMessage('Username '),
+  body('tos').isBoolean().withMessage('')
+], PutUserController);
+
+UserRouter.get("/:userid", 
+  param('userid').isInt().withMessage('User ID'), 
+  GetUserController
+)
+// 
+UserRouter.delete('/:userid', 
+  param('userid').isInt().withMessage('User ID'),
+  DeleteUserController
+);
+
+export default UserRouter;
